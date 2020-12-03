c7e23d375630305ba4e145bb06b26f18e51ebb72
Petrus Batalha <petrus.batalha1@ibm.com>
2020-12-03 08:32:04 -0300

    Checa se a historia existe antes de criar.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ extern crate serde_json;
 extern crate yaml_rust;
 #[macro_use]
 extern crate log;
-use reqwest::{
-    header::{HeaderMap, HeaderValue},
-    Client, Error, StatusCode,
-};
+#[macro_use]
+extern crate lazy_static;
+use reqwest::header::CONTENT_TYPE;
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use serde_json::Value;
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::Read;
@@ -40,12 +40,6 @@ struct Opt {
     /// Log Level
     #[structopt(short = "l", long = "log", default_value = "INFO")]
     log_level: String,
-}
-
-#[derive(Debug, Clone, Serialize)]
-struct IssueBody {
-    fields_meta: FieldsMeta,
-    fields: Fields,
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -84,6 +78,10 @@ struct IssuesResponse {
     issues: Vec<Value>,
 }
 
+lazy_static! {
+    static ref REST_CLIENT: Client = reqwest::Client::new();
+}
+
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
@@ -98,7 +96,7 @@ async fn main() {
 
     // Load Yaml file.
     let yaml = load_yaml(&yaml_path).await;
-    call(&jira_api, &user, &pass, &yaml).await.unwrap();
+    build_req(&jira_api, &user, &pass, &yaml).await.unwrap();
 }
 
 async fn load_yaml(yaml_path: &str) -> Yaml {
@@ -113,37 +111,43 @@ async fn load_yaml(yaml_path: &str) -> Yaml {
     yaml_file[0].clone()
 }
 
-async fn call(
+async fn build_req(
     jira_api_path: &str,
     user: &str,
     pass: &str,
     yaml: &Yaml,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
     let story_uri = format!("{}{}", jira_api_path.clone(), "/issue/");
 
     // Build req body.
+    let req_bodies = build_req_body(user, pass, jira_api_path, &yaml).await;
+    for b in req_bodies.clone() {
+        debug!("BODY {}", b.get("fields").unwrap());
+    }
+    for b in req_bodies {
+        let body = serde_json::to_string(&b).unwrap();
 
-    // let auth_headers = build_auth_headers(user.to_string().clone(), &pass);
-    let req_bodies = build_req_body(user, pass, jira_api_path, &client, &yaml).await;
+        let res = REST_CLIENT
+            .post(&story_uri)
+            .basic_auth(user, Some(&pass))
+            .header(CONTENT_TYPE, "application/json")
+            .body(body.clone())
+            .send()
+            .await?;
 
-    // for b in req_bodies {
-    //     let body = serde_json::to_string(&b).unwrap();
-    //
-    //     let res = client
-    //         .post(&story_uri)
-    //         .headers(auth_headers.clone())
-    //         .body(body.clone())
-    //         .send()
-    //         .await?;
-    //
-    //     if res.status().is_success() {
-    //         info!("Historia {} criada com sucesso", b.fields.summary);
-    //     } else {
-    //         error!("Nao foi possivel criar historia: {:#?}", b.fields.summary);
-    //         debug!("{:#?}", resp);
-    //     }
-    // }
+        if res.status().is_success() {
+            info!(
+                "Historia {:#?} criada com sucesso",
+                b.get("fields").unwrap().get("summary")
+            );
+        } else {
+            error!(
+                "Nao foi possivel criar historia: {:#?}",
+                b.get("fields").unwrap().get("summary")
+            );
+            debug!("{:#?}", res);
+        }
+    }
     Ok(())
 }
 
@@ -151,31 +155,41 @@ async fn build_req_body(
     user: &str,
     pass: &str,
     jira_api: &str,
-    client: &Client,
     yaml: &Yaml,
-) -> Vec<IssueBody> {
+) -> Vec<serde_json::Value> {
     let fields_meta = build_req_meta(&yaml);
 
-    get_existent_stories(user, pass, client, jira_api, &fields_meta.customfield_10101).await;
+    let existent_stories: Vec<String> =
+        get_existent_stories(user, pass, jira_api, &fields_meta.customfield_10101)
+            .await
+            .unwrap();
 
-    let mut req_list: Vec<IssueBody> = Vec::new();
+    let mut req_list: Vec<serde_json::Value> = Vec::new();
 
     // Get yaml block "stories".
     for story in yaml["stories"].as_vec().unwrap() {
         let summary: String = story["name"].as_str().unwrap().to_string();
-        let description: String = story["description"].as_str().unwrap().to_string();
 
-        let req_body = IssueBody {
-            fields_meta: fields_meta.clone(),
-            fields: Fields {
-                summary,
-                description,
-            },
+        if existent_stories.contains(&summary) {
+            error!("'Historia {}' -> Já existe.", summary);
+        } else {
+            let description: String = story["description"].as_str().unwrap().to_string();
+
+            let req_body = json!({
+                "fields": {
+                    "summary": summary,
+                    "description": description,
+                    "project": fields_meta.project,
+                    "issuetype": fields_meta.issuetype,
+                    "labels": fields_meta.labels,
+                    "components": fields_meta.components,
+                    "customfield_10101": fields_meta.customfield_10101,
+                    "customfield_10214": fields_meta.customfield_10214,
+                },
+            });
+            debug!("Req body: {:?}", req_body);
+            req_list.push(req_body.clone());
         };
-
-        req_list.push(req_body.clone());
-
-        debug!("Req body: {:?}", req_body);
     }
     req_list
 }
@@ -183,10 +197,9 @@ async fn build_req_body(
 async fn get_existent_stories(
     user: &str,
     pass: &str,
-    client: &Client,
     jira_api_path: &str,
     epic: &str,
-) -> Result<(), reqwest::Error> {
+) -> Result<Vec<String>, reqwest::Error> {
     let epic_custom_field: i32 = 10101 as i32;
     let search_uri = format!(
         "{}/search?jql=cf[{}]={}",
@@ -195,7 +208,7 @@ async fn get_existent_stories(
 
     debug!("Search uri {:?}", search_uri);
 
-    let issues = client
+    let issues = REST_CLIENT
         .get(&search_uri)
         .basic_auth(user, Some(pass))
         .send()
@@ -206,17 +219,25 @@ async fn get_existent_stories(
     let mut epic_issues: Vec<String> = Vec::new();
 
     for issue in issues.unwrap().issues {
-        epic_issues.push(issue.get("fields").unwrap().get("summary").into());
+        let parsed_issue = issue
+            .get("fields")
+            .unwrap()
+            .get("summary")
+            .unwrap()
+            .to_string()
+            .replace("\"", "")
+            .replace("\\n", "");
+        epic_issues.push(parsed_issue);
     }
 
-    // println!("Search response {:?}", issues.unwrap());
-    Ok(())
+    Ok(epic_issues)
 }
 
 fn build_req_meta(yaml: &Yaml) -> FieldsMeta {
     let yaml_metadata = &yaml["metadata"];
     let epic = yaml_metadata.clone()["epic"].as_str().unwrap().to_string();
     let team = yaml_metadata.clone()["team"].as_str().unwrap().to_string();
+
     let issuetype = IssueType {
         name: "Story".to_string(),
     };
